86d4aaf813b5d5a10b3943c6e72cc21dab280266
Kushaal Singla (KK) <kushaal@appringer.com>
2025-08-15 12:09:01 +0530

    Create install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chmod +x install-php-switchers.sh
+chmod +x install-git-shortcuts.sh
+
+./install-php-switchers.sh
+./install-git-shortcuts.sh
+./install-gdeploy.sh
+
+echo "🎉 All shortcuts installed!"
