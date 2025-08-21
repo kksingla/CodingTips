13e6a6a8fd5def29160390461ab7c4f53a02ab04
Kushaal Singla (KK) <kushaal@appringer.com>
2025-08-21 15:28:53 +0530

    Update install-php-switchers.sh

--- install-php-switchers.sh
+++ install-php-switchers.sh
@@ -9,9 +9,23 @@ brew install shivammathur/php/php@8.3
 cat << 'EOF' >> ~/.zshrc
 
 # PHP Switchers
-php74() { brew unlink php && brew link --overwrite --force php@7.4 && php -v; }
-php82() { brew unlink php && brew link --overwrite --force php@8.2 && php -v; }
-php83() { brew unlink php && brew link --overwrite --force php@8.3 && php -v; }
+php74() {
+  brew unlink php@8.2 php@8.3 2>/dev/null
+  brew link --overwrite --force php@7.4
+  php -v
+}
+
+php82() {
+  brew unlink php@7.4 php@8.3 2>/dev/null
+  brew link --overwrite --force php@8.2
+  php -v
+}
+
+php83() {
+  brew unlink php@7.4 php@8.2 2>/dev/null
+  brew link --overwrite --force php@8.3
+  php -v
+}
 
 EOF
 
